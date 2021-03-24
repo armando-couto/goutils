ad604ae58323b79fdd2f2962ffe773695fca0ee9
Armando Couto <coutoarmando@gmail.com>
2021-03-23 22:00:55 -0300

    Melhorias

--- logger_file.go
+++ logger_file.go
@@ -4,23 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime"
 	"time"
 )
 
 func CreateFileDay(message string) {
-	if runtime.GOOS == "linux" {
-		f, err := os.OpenFile(fmt.Sprint(time.Now().Format("20060102"), ".log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Println(err)
-		}
-		defer f.Close()
-
-		logger := log.New(f, "producao: ", log.Ltime)
-		logger.Println(message)
-	} else {
-		log.Println(message)
+	f, err := os.OpenFile(fmt.Sprint(time.Now().Format("20060102"), ".log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
 	}
+	defer f.Close()
+
+	logger := log.New(f, "producao: ", log.Ltime)
+	logger.Println(message)
 }
 
 func Updates(message string) {
