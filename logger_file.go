33f8a27c5cd707c3b139536025941a64d625418a
Armando Couto <coutoarmando@gmail.com>
2021-04-25 21:28:10 -0300

    Novo método de Info e Error.

--- logger_file.go
+++ logger_file.go
@@ -8,15 +8,26 @@ import (
 )
 
 /*
-	CreateFileDay
+	CreateFileDayInfo o antigo nome era: CreateFileDay
 */
-func CreateFileDay(message string) {
+func CreateFileDayInfo(message string) {
 	f, err := os.OpenFile(fmt.Sprint(time.Now().Format("20060102"), ".log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println(err)
 	}
 	defer f.Close()
 
-	logger := log.New(f, "producao: ", log.Ltime)
+	logger := log.New(f, "INFO\t", log.Ldate|log.Ltime)
 	logger.Println(message)
+}
+
+func CreateFileDayError(message string) {
+	f, err := os.OpenFile(fmt.Sprint(time.Now().Format("20060102"), ".log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	logger := log.New(f, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	logger.Fatalln(message)
 }
