10508e57e0b969d4f18508ec2c34894d39b3de33
Armando Couto <coutoarmando@gmail.com>
2021-05-29 17:37:12 -0300

    Novo métod: CreateFileDayInfoNotDate

--- logger_file.go
+++ logger_file.go
@@ -31,3 +31,14 @@ func CreateFileDayError(message string) {
 	logger := log.New(f, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	logger.Println(message)
 }
+
+func CreateFileDayInfoNotDate(message string) {
+	f, err := os.OpenFile(fmt.Sprint(time.Now().Format("20060102"), ".log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	logger := log.New(f, "", 0)
+	logger.Println(message)
+}
