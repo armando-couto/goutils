ce995aefa3c6746a86cc21ba3a3b2b8206aa4278
Armando Couto <coutoarmando@gmail.com>
2021-06-07 15:20:21 -0300

    Ajustes

--- psql_connection.go
+++ psql_connection.go
@@ -26,7 +26,6 @@ func ConnectionBDPostgreSQL() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
-	db.SetMaxOpenConns(50)
 	db.SetMaxIdleConns(25)
 	db.SetConnMaxLifetime(5 * time.Minute)
 	if err != nil {
