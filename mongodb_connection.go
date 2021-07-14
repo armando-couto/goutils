49a03232abe7c0c3a648a456bbf1a4b1f888a278
Armando Couto <coutoarmando@gmail.com>
2021-07-14 13:10:39 -0300

    Adiciona conecao com MongoDB

---
+++ mongodb_connection.go
@@ -0,0 +1,27 @@
+package goutils
+
+import (
+	"context"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/joho/godotenv/autoload"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func ConnectionMongoDB() *mongo.Database {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var max uint64
+	max = 10
+	clientOptions := options.ClientOptions{MaxPoolSize: &max}
+
+	clientOptions.SetAuth(options.Credential{Username: Godotenv("logger_user"), Password: Godotenv("logger_password")})
+	clientOptions.ApplyURI(Godotenv("logger_url"))
+	client, err := mongo.Connect(ctx, &clientOptions)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	return client.Database("logger_database")
+}
