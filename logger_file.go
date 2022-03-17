f4dfb5c02460ba6fe4689d5b26695411e3b739b4
Armando Couto <coutoarmando@gmail.com>
2022-03-17 17:33:54 -0300

    teste

--- logger_file.go
+++ logger_file.go
@@ -36,7 +36,7 @@ func FormatMessage(message Message) *Message {
 	return &message
 }
 
-func CreateFileDay2(message Message, logger *log.Logger) {
+func CreateFileDay2(message Message) {
 	message.Query = strings.ReplaceAll(message.Query, "\n", "")
 	f, err := os.OpenFile(fmt.Sprint(time.Now().Format("20060102"), ".log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -44,14 +44,9 @@ func CreateFileDay2(message Message, logger *log.Logger) {
 	}
 	defer f.Close()
 
-	logger.Println("dfdsfhdksfsdkfjsdkfsdf")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("dfdsfhdksfsdkfjsdkfsdf")
 	fmt.Println(message)
-}
-
-func FormatMessage2(message Message) (Message, *log.Logger) {
-	logger := log.Logger{}
-	logger.SetFlags(log.LstdFlags | log.Lshortfile)
-	return message, &logger
 }
 
 type MongoObject2 struct {
