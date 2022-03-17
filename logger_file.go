338de483fdbfcbcef5fe6a81c087b124abbca85d
Armando Couto <coutoarmando@gmail.com>
2022-03-17 17:48:17 -0300

    teste

--- logger_file.go
+++ logger_file.go
@@ -36,7 +36,7 @@ func FormatMessage(message Message) *Message {
 	return &message
 }
 
-func CreateFileDay2(message Message, logger log.Logger) {
+func CreateFileDay2(message Message, logger *log.Logger) {
 	message.Query = strings.ReplaceAll(message.Query, "\n", "")
 	f, err := os.OpenFile(fmt.Sprint(time.Now().Format("20060102"), ".log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -48,8 +48,8 @@ func CreateFileDay2(message Message, logger log.Logger) {
 	fmt.Println(message)
 }
 
-func FormatMessage2(message Message) (Message, log.Logger) {
-	logger := log.Logger{}
+func FormatMessage2(message Message) (Message, *log.Logger) {
+	var logger *log.Logger = new(log.Logger)
 	logger.SetFlags(log.LstdFlags | log.Lshortfile)
 	return message, logger
 }
