038de03a71770524486bc93fddc01dd618e53e67
Armando Couto <coutoarmando@gmail.com>
2022-05-16 21:16:14 -0300

    Corrige bug do RandSeq(n)

--- string.go
+++ string.go
@@ -65,6 +65,7 @@ func RemoveSpecialCharacters(value string) string {
 	RandSeq
 */
 func RandSeq(n int) string {
+	rand.Seed(time.Now().UnixNano())
 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 	b := make([]rune, n)
---
+++ string_test.go
@@ -0,0 +1,10 @@
+package goutils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRandSeq(t *testing.T) {
+	fmt.Println(RandSeq(12))
+}
