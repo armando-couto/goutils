7609014428f636abdc83374580464e21564aea48
rayanemarusca <maruscarayane@gmail.com>
2022-09-22 14:07:29 -0300

    novo método de Updload

--- aws_s3.go
+++ aws_s3.go
@@ -1,13 +1,14 @@
 package goutils
 
 import (
-	"bytes"
-	"encoding/base64"
-	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"mime/multipart"
-	"time"
+  "bytes"
+  "encoding/base64"
+  "fmt"
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/service/s3/s3manager"
+  "mime/multipart"
+  "strings"
+  "time"
 )
 
 func UpdloadInS3(file multipart.File, path, fileName string) string {
@@ -138,3 +139,43 @@ func UpdloadInS3Base64Byte(b64 []byte, path, fileName string) string {
 	CreateFileDay(Message{Info: fmt.Sprint("Upload do Arquivo: ", up.UploadID)})
 	return "https://" + myBucket + "." + "s3.amazonaws.com/" + fileName
 }
+
+func UpdloadInS3ArqTxt(texto string, path, fileName string) string {
+  // The session the S3 Uploader will use
+  sess := ConnectAws()
+
+  //fileName = fmt.Sprint(time.Now().Format(LAYOUT_YYYYMMDDHHMMSS), fileName)
+
+  // Caso tenha om PATH ai ele concatena
+  if path != "" {
+    fileName = fmt.Sprint(path, "/", fileName)
+  }
+
+  // Create an uploader with the session and default options
+  uploader := s3manager.NewUploader(sess)
+
+  myBucket := Godotenv("BUCKET_NAME")
+
+  //upload to the s3 bucket
+  up, err := uploader.Upload(&s3manager.UploadInput{
+    Bucket: aws.String(myBucket),
+    ACL:    aws.String("public-read"),
+    Key:    aws.String(fileName),
+    Body:   strings.NewReader(texto),
+  })
+  if err != nil {
+    CreateFileDay(Message{Error: err.Error()})
+    return ""
+  }
+  CreateFileDay(Message{Info: fmt.Sprint("Upload do Arquivo: ", up.UploadID)})
+  return "https://" + myBucket + "." + "s3.amazonaws.com/" + fileName
+}
+
+
+//  myBucket := goutils.Godotenv("BUCKET_NAME")
+//  downloader := s3manager.NewDownloader(goutils.ConnectAws())
+//  _, err := downloader.Download(outFile, &s3.GetObjectInput{
+//    Bucket: aws.String(myBucket),
+//
+//    Key:    aws.String(fileName),
+//  }, Acl)
