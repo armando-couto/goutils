f7f3099d6826cca406282a276f773f0b126f8d1c
Armando Couto <coutoarmando@gmail.com>
2023-04-12 11:05:59 -0300

    Nova conexão ConnectionBDPostgreSQLWithSSL

--- connection_psql.go
+++ connection_psql.go
@@ -11,7 +11,7 @@ import (
 )
 
 /*
-	ConnectionBDPostgreSQL o antigo nome era: ConexaoDB
+ConnectionBDPostgreSQL o antigo nome era: ConexaoDB
 */
 func ConnectionBDPostgreSQL() *sql.DB {
 	var host, dbname, user, password string
@@ -42,8 +42,37 @@ func ConnectionBDPostgreSQL() *sql.DB {
 	return db
 }
 
+func ConnectionBDPostgreSQLWithSSL() *sql.DB {
+	var host, dbname, user, password string
+	var port int
+
+	host = Godotenv("host")
+	dbname = Godotenv("dbname")
+	port, _ = strconv.Atoi(Godotenv("port_banco"))
+	user = Godotenv("user")
+	password = Godotenv("password")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=verify-full", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	db.SetMaxOpenConns(5)
+	db.SetMaxIdleConns(1)
+	db.SetConnMaxLifetime(1 * time.Minute)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		time.Sleep(2 * time.Second)
+		return ConnectionBDPostgreSQL()
+	}
+
+	return db
+}
+
 /*
-	ConnectionBDPostgreSQLORM o antigo nome era: ConexaoBDORM
+ConnectionBDPostgreSQLORM o antigo nome era: ConexaoBDORM
 */
 func ConnectionBDPostgreSQLORM() (DB *gorm.DB) {
 	var host, dbname, user, password string
