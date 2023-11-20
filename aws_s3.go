cc164f76dc17615e7ef348373b79ed837c78d894
rayanemarusca <maruscarayane@gmail.com>
2023-11-20 14:04:45 -0300

    add métodos DownloadFromS3NotPublic e DownloadFromS3Public

--- aws_s3.go
+++ aws_s3.go
@@ -5,6 +5,8 @@ import (
 	"encoding/base64"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"mime/multipart"
@@ -171,8 +173,37 @@ func UpdloadInS3ArqTxt(texto string, path, fileName string) string {
 	return "https://" + myBucket + "." + "s3.amazonaws.com/" + fileName
 }
 
-func DownloadFromS3(fileName, localFilePath string) error {
+func DownloadFromS3Public(fileName, localFilePath string) error {
 	sess := ConnectAws()
+	downloader := s3.New(sess)
+
+	file, err := os.Create(localFilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	bucketName := Godotenv("BUCKET_NAME")
+	_, err = downloader.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(fileName),
+	})
+
+	return err
+}
+
+func DownloadFromS3NotPublic(fileName, localFilePath string) error {
+	accessKey := Godotenv("AWS_ACCESS_KEY_ID")
+	secretKey := Godotenv("AWS_SECRET_ACCESS_KEY")
+
+	// Configuração das credenciais
+	creds := credentials.NewStaticCredentials(accessKey, secretKey, "")
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(Godotenv("AWS_REGION")), // Substitua pela sua região AWS
+		Credentials: creds,
+	})
+
 	downloader := s3.New(sess)
 
 	file, err := os.Create(localFilePath)
